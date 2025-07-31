d90542584e75c0343702aff105bf6aec86c6bc6c
rex.c <rex.c@intteam.net>
2025-07-31 14:27:02 +0800

    init commit

---
+++ app/info/contact/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+function ContactPage() {
+  return (<div>
+    <h1 className='text-7xl'>Home Pfassge</h1>
+    <Link href='/' className='text-blue-500 inline-block mt-8'>Home</Link>
+    </div>)
+}
+
+export default ContactPage;
